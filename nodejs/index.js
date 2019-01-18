8fdfe9faa0a22567b4235fb3bb074c2a2bd58ea7
Beibei Yang <Beibei.Yang@converse.com>
2019-01-18 10:15:17 -0500

    Clean up quotes and update diagram

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,13 +2,13 @@
 // Lambda function to subscribe to SNS topic and publish tiered messages to a Slack channel
 /////////////////////////////////////////
 
-const https = require('https');
-const util = require('util');
-const config = require('./config');
+const https = require("https");
+const util = require("util");
+const config = require("./config");
 
 exports.handler = function(event, context) {
     console.log(JSON.stringify(event, null, 2));
-    console.log('From SNS:', event.Records[0].Sns.Message);
+    console.log("From SNS:", event.Records[0].Sns.Message);
 
     const postData = {
         "channel": config.slack.channel,
@@ -30,8 +30,8 @@ exports.handler = function(event, context) {
       severity = "good";
     }
 
-    console.log('Message: ' + message);
-    console.log('Severity: ' + severity);
+    console.log("Message: " + message);
+    console.log("Severity: " + severity);
 
 
     postData.attachments = [
@@ -42,22 +42,22 @@ exports.handler = function(event, context) {
     ];
 
     var options = {
-        method: 'POST',
+        method: "POST",
         hostname: config.slack.hostname,
         port: config.slack.port,
         path: config.slack.endpoint
     };
 
     var req = https.request(options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
+      res.setEncoding("utf8");
+      res.on("data", function (chunk) {
         context.done(null);
       });
     });
 
     console.log("postData" + postData);
-    req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
+    req.on("error", function(e) {
+      console.log("problem with request: " + e.message);
     });
 
     req.write(util.format("%j", postData));
